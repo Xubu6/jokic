47e9cd4ef1bf33ceba980790fbf919107cefe9ca
Xubu6 <a.x.alxu@gmail.com>
2023-04-10 21:02:52 -0400

    clean up imports

--- api/src/data/scripts/lebron.py
+++ api/src/data/scripts/lebron.py
@@ -4,12 +4,8 @@ from selenium import webdriver
 import mysql.connector
 import chromedriver_autoinstaller
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from ast import literal_eval
 
 def convert(val):
