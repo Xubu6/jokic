941c7ad149f8d2475b52b052433193000a23bc75
Xubu6 <a.x.alxu@gmail.com>
2023-08-22 13:07:48 -0400

    getting everything connected locally

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,12 +4,7 @@ import MainTable from "./components/MainTable.vue";
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>JOKIC</h1>
     <MainTable />
   </div>
 </template>
